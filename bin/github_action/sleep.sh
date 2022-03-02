791e8ec9e583ae3d0fd1e25fd8bf075e8d4ac14b
saipubw <51276909+poor-circle@users.noreply.github.com>
2022-03-02 12:58:28 +0800

    Update sleep.sh

--- bin/github_action/sleep.sh
+++ bin/github_action/sleep.sh
@@ -2,4 +2,4 @@ if [ "$GITHUB_EVENT_NAME" != "workflow_dispatch" ];then
   echo "random sleep 1-10000 seconds." && sleep $[($RANDOM % 10000)+1]
 else
   echo "workflow dispath manually, skip sleeping."
-fi;
+fi
